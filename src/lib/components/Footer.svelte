9b274aa53c667c8de2a2ff5e9a51195c4059b9a6
sisyphi <jordan.m.sibug@gmail.com>
2024-05-09 20:15:45 +0800

    feat(footer): Add Footer component WIP

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+    <!-- <img class="relative z-0 tear-north" src={tear_south_base_offwhite} alt="">
+    <div class="relative z-50 bg-[#F8F1E7]">
+        <div class="">
+            <div class="">
+                <div>
+                    <a on:click={()=>(isMenuOpen = false)} href="/">
+                        <logo class="flex">
+                            <svelte:component this={rt_logo} />
+                        </logo>
+                    </a>
+                </div>
+                <div>
+                    <a class="text-sm text-center hover:underline" href="mailto:maria.ysabel.dinsay@gmail.com">maria.ysabel.dinsay@gmail.com</a>
+                </div>
+                <div class="flex">
+                    <a class="size-6" href="https://www.linkedin.com/in/ysabel-dinsay/"><img src={linkedin_icon_graphic} alt="Linkedin logo"></a>
+                    <a class="size-6" href="https://www.instagram.com/ysabeldinsay/"><img src={ig_icon_graphic} alt="Instagram logo"></a>
+                </div>
+            </div>
+        </div>
+    </div> -->
+</footer>
