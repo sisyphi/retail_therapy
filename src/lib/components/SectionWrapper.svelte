3337fa004e28164fdf1b9ad41b7312f4eb8f50c7
sisyphi <jordan.m.sibug@gmail.com>
2024-05-09 18:51:53 +0800

    test(section wrapper): Remove test for SectionWrapper component

--- src/lib/components/SectionWrapper.svelte
+++ src/lib/components/SectionWrapper.svelte
@@ -5,45 +5,9 @@
 </script>
 
 <section>
-    <img class="transform rotate-180 bg-transparent -mb-0.5 md:-mb-4 lg:-mb-6" src={tear_south_base_offwhite} alt="">
-    <img class="tear-north" src={tear_south_base_offwhite} alt="">
-    <div class="bg-[#F8F1E7]">
+    <img class="relative z-0 transform rotate-180 bg-transparent -mb-0.5 md:-mb-4 lg:-mb-6" src={tear_south_base_offwhite} alt="">
+    <div class="relative z-10 bg-[#F8F1E7]">
         <slot />
     </div>
-    <img class="transform bg-transparent -mt-0.5 md:-mt-4 lg:-mt-6" src={tear_south_base_offwhite} alt="">
-    <img class="tear-south" src={tear_south_base_offwhite} alt="">
+    <img class="relative z-0 transform bg-transparent -mt-0.5 md:-mt-4 lg:-mt-6" src={tear_south_base_offwhite} alt="">
 </section>
-
-<style>
-    .tear-south {
-        background-color: transparent;
-        /* filter: drop-shadow(0 0.3rem 0.6rem black); */
-
-        margin-top: -0.1rem;
-
-        @media(min-width: 768px) {
-            margin-top: -1rem;
-        }
-
-        @media(min-width: 1024px) {
-            margin-top: -1.5rem;
-        }
-    }
-
-    .tear-north {
-        transform: rotate(180deg);
-
-        background-color: transparent;
-        /* filter: drop-shadow(0 -0.3rem 0.6rem black); */
-
-        margin-bottom: -0.1rem;
-
-        @media(min-width: 768px) {
-            margin-bottom: -1rem;
-        }
-
-        @media(min-width: 1024px) {
-            margin-bottom: -1.5rem;
-        }
-    }
-</style>
