173c70b0ad10ad62e85ea7da6494b9146550e654
sisyphi <jordan.m.sibug@gmail.com>
2024-05-13 11:11:26 +0800

    feat: Adds Privacy Policy page

--- src/lib/components/CardWrapper.svelte
+++ src/lib/components/CardWrapper.svelte
@@ -2,8 +2,10 @@
     import tear_south_white from '$lib/images/tear-south-white.png';
 </script>
 
-<img class="relative z-0 rotate-180 bg-transparent -mb-0.5 md:-mb-2" src={tear_south_white} alt="">
-<div class="relative z-10 bg-white">
-    <slot />
-</div>
-<img class="relative z-0 bg-transparent -mt-0.5 md:-mt-2" src={tear_south_white} alt="">
+<section>
+    <img class="relative z-0 rotate-180 bg-transparent -mb-0.5 md:-mb-2" src={tear_south_white} alt="">
+    <div class="relative z-10 bg-white">
+        <slot />
+    </div>
+    <img class="relative z-0 bg-transparent -mt-0.5 md:-mt-2" src={tear_south_white} alt="">
+</section>
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import SectionWrapper from "$lib/components/SectionWrapper.svelte";
+    import CardWrapper from "$lib/components/CardWrapper.svelte";
+</script>
+
+<SectionWrapper>
+    <div class="max-w-2xl py-4 mx-auto my-auto">
+        <CardWrapper>
+            <div class="p-8 sm:px-16">
+                <h1 class="pb-6 text-4xl">Privacy Policy for <br> Retail Therapy.</h1>
+                <p class="pb-4">At Retail Therapy., accessible from https://retailtherapy.netlify.app/, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Retail Therapy. and how we use it.</p>
+                <p class="pb-6">If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Log Files</h2>
+                <p class="pb-4">Retail Therapy. follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information. Our Privacy Policy was created with the help of the <a href="https://www.privacypolicygenerator.org">Privacy Policy Generator</a>.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Privacy Policies</h2>
+                <p class="pb-4">You may consult this list to find the Privacy Policy for each of the advertising partners of Retail Therapy.</p>
+                <p class="pb-4">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Retail Therapy., which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+                <p class="pb-4">Note that Retail Therapy. has no access to or control over these cookies that are used by third-party advertisers.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Third Party Privacy Policies</h2>
+                <p class="pb-4">Retail Therapy.'s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
+                <p class="pb-4">You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Children's Information</h2>
+                <p class="pb-4">Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
+                <p class="pb-4">Retail Therapy. does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Online Privacy Policy Only</h2>
+                <p class="pb-4">This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Retail Therapy. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+                
+                <h2 class="pt-2 pb-2 text-2xl font-bold">Consent</h2>
+                <p class="pb-4">By using our website, you hereby consent to our Privacy Policy and agree to its Terms and Conditions.</p>									
+            </div>
+        </CardWrapper>
+    </div>
+</SectionWrapper>
