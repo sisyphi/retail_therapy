a0a9e2f7d50879a201a7e62927348d7564ddbf1b
sisyphi <jordan.m.sibug@gmail.com>
2024-05-14 13:18:52 +0800

    feat: Adds The Rise of Customization article

---
+++ src/routes/articles/rise-of-customization/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    import featured_article_graphic from '$lib/images/featured-article.png';
+
+    import SectionWrapper from '$lib/components/SectionWrapper.svelte';
+</script>
+
+<SectionWrapper>
+    <div class="relative z-50 gap-4 px-4 py-4 md:py-8 md:flex md:justify-center md:content-center md:items-center">
+        <div class="flex flex-col sm:mx-36 md:mx-44 lg:mx-64 xl:mx-96">
+            <h1 class="pb-6 text-3xl">The rise of customization and why we crave expression </h1>
+            <p class="text-sm">Written by: Ysabel Dinsay | Published on: April 24, 2024 </p>
+
+            <img class="pb-4 m-auto" src={featured_article_graphic} alt="Featured Article (Collage of loose pastel blouses)" width="458" height="407">
+
+            <p class="pb-4 text-base text-left">Crocs, Sunnies Flasks, Viajecito, Louis Vuitton. All of these brands have one thing in common. They all offer customization. </p>
+            <p class="pb-4 text-base text-left">We all want to stand out. And in a world that’s 8 billion people strong, that means maximalism is so in. Gone are the days of decorating your home like a contemporary blank canvas under the guise of simplicity. No. Now we want expression, flavor, and personality. We want to tell people who we are. One way we like to do this in the age of hyperconsumerism is through the objects we own. </p>
+            <p class="pb-4 text-base text-left">One study shows that 90% of consumers are willing to pay up to 25% more for a customized product [3]. And what people pay more for, brands sell more of. </p>
+            <p class="pb-4 text-base text-left">But what attracts us to customization? </p>
+            <h2 class="pb-6 text-2xl font-bold">Function </h2>
+            <p class="pb-4 text-base text-left">Sometimes we just want things for different purposes [1]. Sports brands typically create shoes for different activities like running, golf, football, weightlifting, the list goes on. But a good example of a brand that takes this a step further is New Balance. They offer shoe molds for standard, wide, and extra-wide feet. That simple consideration of other types of feet makes all the difference to some who can’t find athletic shoes that fit them properly. This type of customization wins favor for practical reasons. </p>
+            <h2 class="pb-6 text-2xl font-bold">Identity </h2>
+            <p class="pb-4 text-base text-left">Everyone wants to be unique. </p>
+            <p class="pb-4 text-base text-left">I’m one of them. I got the iPhone 14 in 2022 because they had a purple model. It was so cute and God knows I wanted everyone to know I liked it enough to buy it. </p>
+            <p class="pb-4 text-base text-left">What we have to understand is self-expression is important to the human psyche. We have an intense need to be seen, to be heard. This most likely stems from infanthood. As babies, we cried to convey our need for food and attention. In adulthood, we talk, walk, and dress a certain way to signal to others who we are. Self-expression in line with our authentic selves enhances our individuality and is linked to decreased stress and increased life satisfaction [4]. </p>
+            <p class="pb-4 text-base text-left">While this sounds very individualistic, self-expression is really a tool for connecting with others. Because everyone wants to be unique yes, but not too unique that you don’t below somewhere. </p>
+            <p class="pb-4 text-base text-left">Let me illustrate. I have a purple iPhone. My cousin got the same. We see each other and we go “OMG MINE’s PURPLE TOO!” At that moment, a release of oxytocin bonds us together. We chase this release. We crave it. We look for the human connection we get from shared parts of our identities, even if that part is just a purple iPhone. Because truthfully, our brain doesn’t recognize the difference between purple iPhones and the fact that we are literally cousins. </p>
+            <h2 class="pb-6 text-2xl font-bold">Does customization <span class="italic">really</span> make us happy? </h2>
+            <p class="pb-4 text-base text-left">Like all things related to human behavior, the answer is a big fat it depends. </p>
+            <p class="pb-4 text-base text-left">I still have my purple iPhone and it still makes me happy. So if it truly is something you want then go ahead, design your own Sunnies Flask, pick out your jibitz, and live your life. But remember, at the end of the day, customization gives us the avenue to express ourselves and connect with others. We evolved to have this innate desire because it helps us survive. We signal to others who we are, what we need, what we want, and we bond with others who are similar. That’s valuable and even quite beautiful. </p>
+            <p class="pb-4 text-base text-left">But balance is not consumerism’s strong suit. All over social media, we have seen people fighting over limited edition Stanley Cups, Trader Joe's tote bags, and rare Starbucks Tumblers. We live in a world that’s increasingly isolating us. Globally, nearly 1 in 4 people report feeling lonely [5]. And despite the COVID-19 pandemic being over, the numbers are not getting much better [2]. We have to face it. Loneliness is an epidemic. When it becomes so difficult to connect with others and foster strong bonds, it becomes easy to get lost in heavily defining ourselves by the products we own. The brief moment of elation in securing the item you want and sharing on your socials will pass and the emptiness that a healthy social life should fill will come back and you will look for another item on the shelf that will temporarily fill that void. </p>
+            <p class="pb-4 text-base text-left">I can enjoy my purple iPhone but understanding how it makes me happy and doing that thing instead is how I can have a healthier relationship with consumerism. Like I schedule a few dates with my cousin when we’re in the same city. And I’ll only buy clothes in colors and silhouettes that I like and are authentic to me. </p>
+            <p class="pb-4 text-base text-left">As always, it pays to be a smarter consumer. </p>
+
+            <!-- <ol class="" type="1">
+                <li>Allsopp, Josh. “The Psychology of Personalisation: Why We Crave Customised Products.” LinkedIn, 4 July 2019, www.linkedin.com/pulse/psychology-personalisation-why-we-crave-customised-products-allsopp/. </li>
+                <li>Cigna Corporation. “The Loneliness Epidemic Persists: A Post-Pandemic Look at the State of Loneliness among U.S. Adults.” The Cigna Group Newsroom, 26 May 2022, newsroom.thecignagroup.com/loneliness-epidemic-persists-post-pandemic-look. </li>
+                <li>Guevara, Paula. “Benefits of Customization: Why Consumers Love the Power of Personalization.” ConfigureID, 30 Nov. 2022, configureid.com/2021/09/23/benefits-of-customization-why-consumers-love-the-power-of-personalization/. </li>
+                <li>Hodge, Janine. “What Is Self-Expression and Why Is It so Important?” Counselling Directory, Counselling Directory, 24 June 2022, www.counselling-directory.org.uk/memberarticles/what-is-self-expression-and-why-is-it-so-important. </li>
+                <li>Nicioli, Taylor. “The Loneliness Epidemic: Nearly 1 in 4 Adults Feel Lonely, New Survey Finds.” CNN, Cable News Network, 24 Oct. 2023, edition.cnn.com/2023/10/24/health/lonely-adults-gallup-poll-wellness/index.html. </li>
+            </ol> -->
+        </div>
+    </div>
+</SectionWrapper>
